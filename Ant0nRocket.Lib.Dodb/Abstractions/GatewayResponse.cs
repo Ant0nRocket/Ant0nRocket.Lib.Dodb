ba2c10fdd3d947c1614c1b4e9ea68b4ee03214b2
Anton Stephanyak <anton.stephanyak@gmail.com>
2022-03-18 18:51:05 +0500

    [rem] DateCreated removed from Responses

--- Ant0nRocket.Lib.Dodb/Abstractions/GatewayResponse.cs
+++ Ant0nRocket.Lib.Dodb/Abstractions/GatewayResponse.cs
@@ -2,8 +2,6 @@
 {
     public abstract class GatewayResponse
     {
-        public DateTime DateCreatedUtc = DateTime.UtcNow;
-
         public virtual string Message { get; set; } = string.Empty;
     }
 }
