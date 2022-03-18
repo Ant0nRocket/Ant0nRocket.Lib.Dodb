66cacec2121f687c6e766fba43a43d5c51a9c4cb
Anton Stephanyak <anton.stephanyak@gmail.com>
2022-03-18 16:46:30 +0500

    [feat] Message are now virtual in GatewatResponse [feat] Version 1.1.6

--- Ant0nRocket.Lib.Dodb/Abstractions/GatewayResponse.cs
+++ Ant0nRocket.Lib.Dodb/Abstractions/GatewayResponse.cs
@@ -1,17 +1,9 @@
-﻿using Ant0nRocket.Lib.Std20.Logging;
-
-using System.Runtime.CompilerServices;
-
-namespace Ant0nRocket.Lib.Dodb.Abstractions
+﻿namespace Ant0nRocket.Lib.Dodb.Abstractions
 {
     public abstract class GatewayResponse
     {
         public DateTime DateCreatedUtc = DateTime.UtcNow;
 
-        public GatewayResponse WithLogging([CallerMemberName] string methodName = default, LogLevel logLevel = LogLevel.Info)
-        {
-            Logger.Log(this.ToString(), logLevel, methodName);
-            return this;
-        }
+        public virtual string Message { get; set; } = string.Empty;
     }
 }
