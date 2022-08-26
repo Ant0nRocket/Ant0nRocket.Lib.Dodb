7c7a8d7669977eb059db439b896d85c083e5ce57
Ant0nRocket <anton.stephanyak@gmail.com>
2022-08-26 18:24:18 +0500

    [v] 1.4.2

--- Ant0nRocket.Lib.Dodb/Entities/EntityBase.cs
+++ Ant0nRocket.Lib.Dodb/Entities/EntityBase.cs
@@ -1,7 +1,10 @@
-﻿namespace Ant0nRocket.Lib.Dodb.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ant0nRocket.Lib.Dodb.Entities
 {
     public abstract class EntityBase
     {
+        [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
 
         public virtual DateTime DateCreatedUtc { get; set; } = DateTime.UtcNow;
