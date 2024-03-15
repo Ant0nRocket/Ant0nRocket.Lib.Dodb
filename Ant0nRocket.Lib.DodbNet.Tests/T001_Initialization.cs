e4be3de4d19e977eaad13c69cdc903257feec665
StefanyakAY <StefanyakAY@PTO-ENGINEER-1>
2024-03-15 13:42:23 +0500

    [r] Ant0nRocket.Lib now added directly into project

---
+++ Ant0nRocket.Lib.DodbNet.Tests/T001_Initialization.cs
@@ -0,0 +1,16 @@
+namespace Ant0nRocket.Lib.DodbNet.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
